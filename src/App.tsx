fcf43c66cf177f91b0971660abca8e3bfaa5483e
nbabcock <nathan.r.babcock@gmail.com>
2022-09-01 16:18:23 -0500

    feat(routes): Add react-router-dom and App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,14 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Home from './Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
-
-export default App
